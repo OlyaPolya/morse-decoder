4c2b5de3f77be831881448c5ef390ee8823372de
Olya_Polyanskaya <polyanskaya3000@gmail.com>
2022-02-12 20:39:13 +0700

    feat: finish project

--- src/index.js
+++ src/index.js
@@ -38,8 +38,52 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let str = '';
+    let res = '';
+    let num = '';
+    let comb = '';
+    let sym = '';
+    let letter = '';
+  
+    for (let i = 0; i < expr.length / 10; i++){
+      str = expr.slice(i * 10, (i + 1) * 10);
+     // console.log(str);
+      if (str === '**********') {
+        res = res + ' ';
+        continue;
+      }
+      for (let j = 4; j >= 0; j--) {
+        num = str.slice(j * 2, (j + 1) * 2);
+        if (num === '10') {
+          sym = '.';
+        }
+        if (num === '11') {
+          sym = '-';
+        }
+        if (num === '00') {
+          break;
+        }
+        comb = sym + comb;
+        
+      }
+  
+     // console.log(comb);
+  
+      for (let item in MORSE_TABLE) {
+        if (comb === item) {
+          letter = MORSE_TABLE[item];
+          console.log(letter);
+        }
+  
+      }
+      res = res + letter;
+      comb = '';
+    }
+    
+    //console.log(res);
+    return res;
 }
+
 
 module.exports = {
     decode
